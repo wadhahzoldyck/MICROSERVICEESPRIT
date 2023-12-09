168b29da258c059395bfd072bb58751b68c2e6d3
Akramhn <akram.hadjnaser@esprit.tn>
2023-12-09 16:03:00 +0100

    first akram commit

---
+++ MicroServicePlantExercice/src/main/java/esprit/microservice/microserviceplantexercice/MicroServicePlantExerciceApplication.java
@@ -0,0 +1,13 @@
+package esprit.microservice.microserviceplantexercice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicePlantExerciceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServicePlantExerciceApplication.class, args);
+    }
+
+}
