bd2cdf87c62ca2e7c7f12d5593089dc7f04271d6
wadhah naggui <wadhah.naggui@esprit.tn>
2023-12-09 15:10:34 +0100

    Service

---
+++ SalleService/src/main/java/com/example/salleservice/SalleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.salleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalleServiceApplication.class, args);
+    }
+
+}
