537af27e5920fa491448a3583d76dd5f4f33bf5e
wadhah naggui <wadhah.naggui@esprit.tn>
2023-12-11 23:11:44 +0100

    circuitBreaker

--- SalleService/src/main/java/com/example/salleservice/model/Coach.java
+++ SalleService/src/main/java/com/example/salleservice/model/Coach.java
@@ -1,13 +1,13 @@
 package com.example.salleservice.model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
 public class Coach {
 
     long idCoach ;
