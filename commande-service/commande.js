51f5f56ae099907a996e7c4a6b345ed98803aeba
Ahmed bel haj boubaker <Ahmed.belhajboubaker@esprit.tn>
2023-12-12 17:09:25 +0100

    integration docker cpmpose

---
+++ commande-service/commande.js
@@ -0,0 +1,18 @@
+export const commandeData = [
+    {
+      id: 1,
+      client: 'ClientA',
+      produits: ['ProduitA', 'ProduitB'],
+      montantTotal: 150,
+      statut: 'En cours'
+    },
+    {
+      id: 2,
+      client: 'ClientB',
+      produits: ['ProduitC'],
+      montantTotal: 75,
+      statut: 'Livré'
+    }
+    // Add more commands if needed
+  ];
+  
